cfc24850537306b317942352fe239a8a7940421e
Canter Daniel <83081029+The1Dani@users.noreply.github.com>
2025-10-30 10:18:48 +0200

    fix backend problem and implement fetch_all_keys()

--- backend/api/retrieve_data.py
+++ backend/api/retrieve_data.py
@@ -5,6 +5,13 @@ from typing import Optional
 # Usage: python series.py <contour_id>
 engine = create_engine("postgresql://postgres:11111@localhost:5433/postgres")
 
+def get_all_keys():
+    sql = text("SELECT contour_id FROM interpolated.contour ORDER BY contour_id ASC")
+    with engine.connect() as conn:
+        result = conn.execute(sql).fetchall()
+    #transform from row to list[int]
+    result = [r[0] for r in result]
+    return list(result)
 
 def fetch_hourly_imports(contour_id: int, day: Optional[int] = None, schema: Optional[str] = ""):
     """Return rows (contour_id, clock, energy_import) for the given contour_id
